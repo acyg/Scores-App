01f3e833c3ad195e5e6bf54f2b4c69fb1ea5cbb0
Deneb Chen <dchen9010@gmail.com>
2018-04-13 22:41:26 -0700

    Opened new branch for switching to use Google Firebase

--- app/src/main/java/com/example/a_cyg/scores/AddScoreActivity.java
+++ app/src/main/java/com/example/a_cyg/scores/AddScoreActivity.java
@@ -10,10 +10,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class AddScoreActivity extends AppCompatActivity {
+    private FirebaseDatabase database;
+    private DatabaseReference dataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,23 +44,27 @@ public class AddScoreActivity extends AppCompatActivity {
                             .setAction("Action", null).show();
                 } else {
                     try {
-                        JSONObject inputObj = new JSONObject();
-                        inputObj.put("name", name);
-                        inputObj.put("score", Integer.parseInt(score));
+                        HashMap<String, Object> data = new HashMap<String, Object>();
+                        data.put("name", name);
+                        data.put("score", Integer.parseInt(score));
+                        data.put("ip-address", Utils.getIPAddress(true));
 
-                        final View fView = view;
-                        new DatabaseTask(DatabaseTask.METHOD_POST, new MainActivity.dataCallback() {
-                            @Override
-                            public void onResult(JSONArray jsonArray) {
-                                try {
-                                    JSONObject result = (JSONObject) ((JSONObject) jsonArray.get(0)).get("result");
-                                    Snackbar.make(fView, (String) result.get("message"), Snackbar.LENGTH_LONG)
-                                            .setAction("Action", null).show();
-                                } catch(Exception e) {
+                        Calendar cal = Calendar.getInstance();
+                        int year = cal.get(Calendar.YEAR);
+                        int month = (cal.get(Calendar.MONTH) + 1);
+                        int day = cal.get(Calendar.DAY_OF_MONTH);
+                        data.put("date", year + "-"
+                                + (month > 9 ? month : "0" + month) + "-"
+                                + day);
 
-                                }
-                            }
-                        }, inputObj).execute();
+                        database = FirebaseDatabase.getInstance();
+                        dataRef = database.getReference("scores/tetris");
+
+                        String key = dataRef.push().getKey();
+                        dataRef.child(key).setValue(data);
+
+                        Snackbar.make(view, "Score uploaded.", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
                     } catch (Exception e) {
                         Log.e("AddScoreActivity", e.getMessage());
                     }
--- app/src/main/java/com/example/a_cyg/scores/MainActivity.java
+++ app/src/main/java/com/example/a_cyg/scores/MainActivity.java
@@ -3,22 +3,36 @@ package com.example.a_cyg.scores;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private dataCallback mDataCallback;
+    private FirebaseDatabase database;
+    private DatabaseReference dataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +49,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton refresh = (FloatingActionButton) findViewById(R.id.refresh);
-        refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DatabaseTask(DatabaseTask.METHOD_GET, mDataCallback).execute();
-            }
-        });
-
         mRecyclerView = (RecyclerView) findViewById(R.id.content_list);
 
         // use this setting to improve performance if you know that changes
@@ -53,20 +59,62 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mDataCallback = new dataCallback(){
-            @Override
-            public void onResult(JSONArray jsonArray) {
-                mAdapter = new ScoreAdapter(jsonArray);
-                mRecyclerView.setAdapter(mAdapter);
-            }
-        };
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        new DatabaseTask(DatabaseTask.METHOD_GET, mDataCallback).execute();
+        //new DatabaseTask(DatabaseTask.METHOD_GET, mDataCallback).execute();
+        database = FirebaseDatabase.getInstance();
+        dataRef = database.getReference("scores/tetris");
+
+        dataRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+                    String jstring = dataSnapshot.getValue().toString();
+                    JSONObject jobj = new JSONObject(jstring);
+
+                    Iterator<String> keys = jobj.keys();
+                    JSONArray jary = new JSONArray();
+
+                    while(keys.hasNext()) {
+                        jary.put(jobj.get(keys.next()));
+                    }
+
+                    JSONObject[] _jary = new JSONObject[jary.length()];
+
+                    for(int i = 0;i < jary.length();i++) {
+                        _jary[i] = (JSONObject) jary.get(i);
+                    }
+
+                    Comparator<JSONObject> cmp = new Comparator<JSONObject>() {
+                        @Override
+                        public int compare(JSONObject o1, JSONObject o2) {
+                            try {
+                                return (Integer) o2.get("score") - (Integer) o1.get("score");
+                            } catch (Exception e) {
+                                return 0;
+                            }
+                        }
+                    };
+
+                    Arrays.sort(_jary, cmp);
+                    jary = new JSONArray(_jary);
+
+                    mAdapter = new ScoreAdapter(jary);
+                    mRecyclerView.setAdapter(mAdapter);
+
+                } catch (Exception e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
     }
 
     public interface dataCallback {
